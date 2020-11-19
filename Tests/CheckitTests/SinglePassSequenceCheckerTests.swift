85f9ce34dd25e8f7fb8b07a67189876ab94a607b
Karl <5254025+karwa@users.noreply.github.com>
2020-11-19 15:16:34 +0100

    Disable uncatchable test

--- Tests/CheckitTests/SinglePassSequenceCheckerTests.swift
+++ Tests/CheckitTests/SinglePassSequenceCheckerTests.swift
@@ -11,14 +11,14 @@ extension SinglePassSequenceTests {
         SinglePassSequenceChecker.check(0..<5) { sequence in sequence.reversed() }
     }
     
-    func testBadAlgorithm() {
+  // TODO: Try to catch this assertion.
+    func __disabled_testBadAlgorithm() {
         func badAlgorithm<S: Sequence>(_ s: S) -> S.Element
             where S.Element: Comparable, S.Element: AdditiveArithmetic {
             let min = s.min()!
             let max = s.max()!
             return min + max
         }
-        // TODO: Try to catch this assertion.
         SinglePassSequenceChecker.check(0..<5) { sequence in
                 badAlgorithm(sequence)
         }
